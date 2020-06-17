010bde45ecefa0e960411bc3f877f1dfeec7e36a
Tyson Williams <tyson.williams@blocherconsulting.com>
2020-06-16 22:54:59 -0500

    removed everything specific to gh-pages

--- .codedoc/config.ts
+++ .codedoc/config.ts
@@ -14,9 +14,9 @@ export const config = /*#__PURE__*/configuration({
   dest: {
     namespace: '/coding-blog-boilerplate',    // --> change this if you want to also deploy to GitHub Pages
     html: 'dist',
-    assets: process.env.GITHUB_BUILD === 'true' ? 'dist' : '.',
-    bundle: process.env.GITHUB_BUILD === 'true' ? 'bundle' : 'dist/bundle',
-    styles: process.env.GITHUB_BUILD === 'true' ? 'styles' : 'dist/styles',
+    assets: '.',
+    bundle: 'dist/bundle',
+    styles: 'dist/styles',
   },
   page: {
     title: {
