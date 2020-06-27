3e5a86889050a1a13ae4be919902e59ff7feb13b
Tyson Williams <tyson.williams@blocherconsulting.com>
2020-06-27 12:07:33 -0500

    added dev dependency source-map-loader

--- .codedoc/serve.ts
+++ .codedoc/serve.ts
@@ -9,6 +9,15 @@ import { installTheme$ } from './content/theme';
 const root = join(__dirname, '../');
 
 serve(root, config, content, installTheme$, {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+    ],
+  },
   resolve: {
     modules: ['.codedoc/node_modules']
   },
