8a905f4bf3826b60962ca4a4e9aeb8f3a5758936
Tyson Williams <tyson.d.w@gmail.com>
2020-07-27 12:33:08 -0500

    added Type invariance section to Identity as As post

---
+++ src/code/IdentityAsAs/Project/6_TypeInvariance.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace TypeInvariance {
+
+  class Sandbox {
+    public Task<string> taskString = Task.FromResult("");
+    public void Foo() => Bar(taskString);
+    //public void Foo() => Bar(taskString.ContinueWith(x => x.As<object>()));
+    public void Bar(Task<object> _) { }
+  }
+}
